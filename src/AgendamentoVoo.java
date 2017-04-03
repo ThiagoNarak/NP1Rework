5d00bf7813ef85ab8d58279d6047be77e1b3580f
Thiago rodrigo <thiago.workshop.c@gmail.com>
2017-04-03 13:05:27 -0300

    1

---
+++ src/AgendamentoVoo.java
@@ -0,0 +1,65 @@
+/**
+ * Created by thiago on 03/04/2017.
+ */
+public class AgendamentoVoo {
+    public static void main(String[] args) {
+
+
+        System.out.println("MENU");
+        System.out.println("a- Comprar passagem");  /*
+        ESCOLHER ORIGEM DESTINO.
+        ESCOLHER DATA DO VOO.
+        ESCOLHER CLASSE ECONOMY,FIRSTCLASS,BUSINESS
+        */
+        switch (menu){
+            case 1:
+                System.out.println("digite origem");
+                System.out.println("digite destino");
+                System.out.println("digite a classe");
+                System.out.println("1-FirstClass");
+                System.out.println("2-Business");
+                System.out.println("3-Economy");
+                switch (classe){
+                    case 1:
+                        break;
+                    case 1: break;
+                    case 1: break;
+                }
+                break;
+
+            case 1: break;
+            case 1: break;
+            case 1: break;
+            case 1: break;
+        }
+        System.out.println("b- Verificar voos disponiveis");
+        /*
+        imprimir voos disponiveis da compania com hora e valor
+        */
+        System.out.println("d- Mos dos assentos");
+        /*
+        mostrar assentos da classe determinada para voo X horario X
+         */
+        System.out.println("f- Cadastrar passageiros");
+        /*
+        digitar o nome sobrenome cpf
+         */
+        System.out.println("e- Emititrar mapar catao de embarque");
+        /*
+        nome cia
+        origem destino
+        classe
+        prioridade
+        data horario
+        setor aeroporto: setor 1
+        embarque portao 1
+
+         */
+        System.out.println("c- Configurar Voo");
+        System.out.println("j- Imprimir passageiros do voo (EXTRA)");
+        System.out.println("i- Cancelamento da viagem (EXTRA)");
+
+        System.out.println("g- Cadatrar tripulantes");
+
+    }
+}
